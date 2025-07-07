f33c46619e3953f2d7456d473ef1bdd935d187b3
DevAlissu <alison.silva8741@gmail.com>
2025-07-07 02:05:11 -0400

    refactor estrutura

--- frontend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.list_cars, name='list_cars'),
-    path('criar/',views.create_car, name='create_car'),
-    path('edit/<int:car_id>/', views.edit_car, name='edit_car'),
-    path('excluir/<int:car_id>/', views.delete_car, name='delete_car'),
-]
